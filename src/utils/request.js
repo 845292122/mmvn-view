6b685b873c6c0f5bc8647437d0ce77268ae61361
edison <845292122@qq.com>
2024-06-26 12:20:07 +0800

    集成axios

---
+++ src/utils/request.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const service = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_API,
+  // timeout: 10000
+})
+
+// 请求拦截器
+service.interceptors.request.use(
+  (req) => {
+    // TODO 如果存在token,将token放在请求头中
+    return req
+  },
+  (err) => {
+    Promise.reject(err)
+  }
+)
+
+// 响应拦截器
+service.interceptors.response.use(
+  (resp) => {
+    // TODO 响应处理
+  },
+  (err) => {
+    // TODO 响应异常处理
+  }
+)
